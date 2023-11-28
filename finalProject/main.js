6916a4656bc0c057bad73f9baf29b2e44dc329ff
leonard <leonardsantos.contact@gmail.com>
2023-11-28 15:03:04 -0500

    update to final project

--- finalProject/main.js
+++ finalProject/main.js
@@ -6,18 +6,71 @@ const width = window.innerWidth * 0.7,
 /* LOAD DATA */
 
 /* LOAD DATA */
-d3.csv('../data/MoMA_distributions.csv', d3.autoType)
+d3.csv('../data/artists.csv', d3.autoType)
   .then(data => {
-  console.log('data :>> ', data);
+    data.forEach(d => {
+      d.year = d3.timeParse('%Y')(d.year); 
+    });
 
   // SCALES
+  const xScale = d3.scaleTime()
+    .domain(d3.extent(data, d => d.year))
+    .range([margin.left, width - margin.right]);
+
+  const yScale = d3.scaleLinear()
+    .domain([d3.min(data,function(d){return d.Gender;}) - 50, d3.max(data,function(d){return d.Gender;}) + 50])
+    .range([height - margin.bottom, margin.top]);
 
   // CREATE SVG ELEMENT
+  const svg = d3.select("#container")
+    .append("svg")
+    .attr("height", height)
+    .attr("width", width)
+    .style("background-color", "aliceblue")
 
   // BUILD AND CALL AXES
+  const xAxis = d3.axisBottom(xScale)
+    .tickFormat(d3.timeFormat('%Y')); 
 
-  // LINE GENERATOR FUNCTION
+  const xAxisGroup = svg.append("g")
+    .attr("class", "xAxis")
+    .attr("transform", `translate(0, ${height - margin.bottom})`)
+    .call(xAxis);
 
-  // DRAW LINE
+  const yAxis = d3.axisLeft(yScale);
 
-});
+  const yAxisGroup = svg.append("g")
+    .attr("class", "yAxis")
+    .attr("transform", `translate(${margin.left}, 0)`)
+    .call(yAxis);
+
+  // TEXT
+    svg.append("text")
+    .attr("x", width / 2)
+    .attr("y", height - 10)
+    .style("text-anchor", "middle")
+    .text("Year");
+
+  svg.append("text")
+    .attr("x", -height / 2)
+    .attr("y", margin.left / 4)
+    .attr("transform", "rotate(-90)")
+    .style("text-anchor", "middle")
+    .text("Total # of Artists");
+
+  // AREA GENERATOR FUNCTION
+  const areaGen = d3.area()
+    .x(d => xScale(d.year))
+    .y0(height - margin.bottom)
+    .y1(d => yScale(d.Gender));
+
+  // DRAW AREA
+  svg.append("path")
+    .datum(data)
+    .attr("class", "area")
+    .attr("d", areaGen)
+    .attr("fill", "red")
+    .attr("stroke", "black")
+    .attr("stroke-width", "1.5")
+
+  });
