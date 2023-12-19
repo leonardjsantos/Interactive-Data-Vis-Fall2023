c20275a6695f4b964162570e3609e82b3f17bb32
leonard <leonardsantos.contact@gmail.com>
2023-12-18 21:50:45 -0500

    finalProject_finalUpdate1

--- finalProject/main.js
+++ finalProject/main.js
@@ -1,10 +1,10 @@
 /* CONSTANTS AND GLOBALS */
-const width = window.innerWidth * 0.7,
+const width = window.innerWidth * 0.8,
   height = window.innerHeight * 0.7,
   margin = { top: 40, bottom: 60, left: 80, right: 40 };
 
 /* LOAD DATA */
-d3.csv('final_artists1.csv', d3.autoType)
+d3.csv('artistsSummary.csv', d3.autoType)
   .then(data => {
     data.forEach(d => {
       d.year = d3.timeParse('%Y')(d.year); 
@@ -16,7 +16,7 @@ d3.csv('final_artists1.csv', d3.autoType)
     .range([margin.left, width - margin.right]);
 
   const yScale = d3.scaleLinear()
-    .domain([d3.min(data,function(d){return d.total;}), d3.max(data,function(d){return d.total;}) + 50])
+    .domain([0, d3.max(data,function(d){return d.total;}) + 20])
     .range([height - margin.bottom, margin.top]);
 
   // CREATE SVG ELEMENT
@@ -42,8 +42,57 @@ d3.csv('final_artists1.csv', d3.autoType)
     .attr("transform", `translate(${margin.left}, 0)`)
     .call(yAxis);
 
+  // LEGEND
+  svg.append("rect")
+    .attr("x", 100)
+    .attr("y", 100)
+    .attr("width", 10)
+    .attr("height", 10)
+    .attr("fill", "blue")
+    .attr("stroke", "black")
+    .attr("stroke-width", 1);
+
+  svg.append("rect")
+    .attr("x", 100)
+    .attr("y", 120)
+    .attr("width", 10)
+    .attr("height", 10)
+    .attr("fill", "red")
+    .attr("stroke", "black")
+    .attr("stroke-width", 1);
+  
+  svg.append("rect")
+    .attr("x", 100)
+    .attr("y", 140)
+    .attr("width", 10)
+    .attr("height", 10)
+    .attr("fill", "yellow")
+    .attr("stroke", "black")
+    .attr("stroke-width", 1);
+
   // TEXT
-    svg.append("text")
+  svg.append("text")
+    .attr("x", 115)
+    .attr("y", 109)
+    .attr("font-size", 12)
+    .attr("fill", "black")
+    .text("Male");
+
+  svg.append("text")
+    .attr("x", 115)
+    .attr("y", 130)
+    .attr("font-size", 12)
+    .attr("fill", "black")
+    .text("Female");
+
+  svg.append("text")
+    .attr("x", 115)
+    .attr("y", 150)
+    .attr("font-size", 12)
+    .attr("fill", "black")
+    .text("Non-Binary");
+
+  svg.append("text")
     .attr("x", width / 2)
     .attr("y", height - 10)
     .style("text-anchor", "middle")
@@ -56,11 +105,17 @@ d3.csv('final_artists1.csv', d3.autoType)
     .style("text-anchor", "middle")
     .text("Total # of Art Pieces Made");
 
+  svg.append("text")
+    .attr("x",100)
+    .attr("y", 70)
+    .attr("font-size", 15)
+    .text("Hover over each area to find the highest number of artwork made by an artist in a single year.");
+
   // AREA GENERATOR FUNCTION
   const areaGen1 = d3.area()
     .x(d => xScale(d.year))
     .y0(height - margin.bottom)
-    .y1(d => yScale(d.Male));
+    .y1(d => yScale(d.Male + d.Female + d.NonBinary));
 
   // DRAW AREA
   svg.append("path")
@@ -70,12 +125,26 @@ d3.csv('final_artists1.csv', d3.autoType)
     .attr("fill", "blue")
     .attr("stroke", "black")
     .attr("stroke-width", "1")
+    .on("mouseover", function(d) {
+      d3.select(this).style("fill", d3.select(this).attr('stroke'))
+          .attr('fill-opacity', 0.5);
+          svg.append("text")
+            .attr("x",150)
+            .attr("y", 200)
+            .attr("font-size", 15)
+            .text("Male artists made 141 pieces in 1943.");
+    })                  
+    .on("mouseout", function(d) {
+      d3.select(this).style("fill", "blue")
+          .attr('fill-opacity', 1);
+    });
 
   // AREA GENERATOR FUNCTION
   const areaGen = d3.area()
     .x(d => xScale(d.year))
     .y0(height - margin.bottom)
-    .y1(d => yScale(d.Female));
+    .y1(d => yScale(d.Female + d.NonBinary));
+  
 
   // DRAW AREA
   svg.append("path")
@@ -85,6 +154,19 @@ d3.csv('final_artists1.csv', d3.autoType)
     .attr("fill", "red")
     .attr("stroke", "black")
     .attr("stroke-width", "1")
+    .on("mouseover", function(d) {
+      d3.select(this).style("fill", d3.select(this).attr('stroke'))
+          .attr('fill-opacity', 0.5);
+          svg.append("text")
+            .attr("x",150)
+            .attr("y", 230)
+            .attr("font-size", 15)
+            .text("Female artists made 43 pieces in 1946.");
+    })                  
+    .on("mouseout", function(d) {
+      d3.select(this).style("fill", "red")
+          .attr('fill-opacity', 1);
+    });
 
   // AREA GENERATOR FUNCTION
   const areaGen2 = d3.area()
@@ -100,6 +182,19 @@ d3.csv('final_artists1.csv', d3.autoType)
     .attr("fill", "yellow")
     .attr("stroke", "black")
     .attr("stroke-width", "1")
+    .on("mouseover", function(d) {
+      d3.select(this).style("fill", d3.select(this).attr('stroke'))
+          .attr('fill-opacity', 0.5);
+          svg.append("text")
+            .attr("x",150)
+            .attr("y", 260)
+            .attr("font-size", 15)
+            .text("Non-Binary artists made 1 piece in 1973, 1983, and 1987.");
+    })                  
+    .on("mouseout", function(d) {
+      d3.select(this).style("fill", "yellow")
+          .attr('fill-opacity', 1);
+    });
 
   
 
